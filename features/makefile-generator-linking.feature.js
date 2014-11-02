7ce8d6c159e180d957fdb7f509aa372bf780aa34
Jesper Oskarsson <jesosk@gmail.com>
2014-11-02 18:22:43 +0100

    Refactored integration tests and added linking test

---
+++ features/makefile-generator-linking.feature.js
@@ -0,0 +1,53 @@
+
+// sea-strap.js - A "build anywhere" C/C++ makefile/project generator.
+// Written in 2014 by Jesper Oskarsson jesosk@gmail.com
+//
+// To the extent possible under law, the author(s) have dedicated all copyright
+// and related and neighboring rights to this software to the public domain worldwide.
+// This software is distributed without any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication along with this software.
+// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+var should = require('should');
+var shell = require('shelljs');
+var util = require('./makefile-generator.util.js');
+
+describe('makefile-generator-linking', function () {
+    beforeEach(function (done) {
+        util.setupEnvironment(function () {
+            shell.mkdir('include');
+            shell.mkdir('source');
+
+            /* Includes and linking */
+            ("#include <stdio.h>\n"
+            +"#include <mylibrary.h>\n"
+            +"int main(int argc, char** argv) {\n"
+            +"  return (add(23, 88) == 111) ? 0 : -1;\n"
+            +"}\n").to("linked.c");
+
+            ("#include \"../include/mylibrary.h\"\n"
+            +"int add(int a, int b) {\n"
+            +"  return a + b;\n"
+            +"}\n").to("source/mylibrary.c");
+
+            ("int add(int a, int b);\n").to("include/mylibrary.h");
+            
+            done();
+        });
+    });
+
+    afterEach(util.teardownEnvironment);
+
+    it('should compile and link correctly given several source files and includes', function (done) {
+        util.compileAndRunConfig({
+            files: [
+                'linked.c',
+                'source/mylibrary.c'
+            ],
+            includePaths: [
+                'include'
+            ]
+        }, './linked', done);
+    });
+});
---
+++ features/makefile-generator-simple.feature.js
@@ -0,0 +1,47 @@
+
+// sea-strap.js - A "build anywhere" C/C++ makefile/project generator.
+// Written in 2014 by Jesper Oskarsson jesosk@gmail.com
+//
+// To the extent possible under law, the author(s) have dedicated all copyright
+// and related and neighboring rights to this software to the public domain worldwide.
+// This software is distributed without any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication along with this software.
+// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+var should = require('should');
+var shell = require('shelljs');
+var util = require('./makefile-generator.util.js');
+
+describe('makefile-generator-simple', function () {
+    beforeEach(function (done) {
+        util.setupEnvironment(function () {
+            /* Single file */
+            ("#include <stdio.h>\n"
+            +"int main(int argc, char** argv) {\n"
+            +"  return 0;\n"
+            +"}\n").to("simple.c");
+            
+            done();
+        });
+    });
+    
+    afterEach(util.teardownEnvironment);
+
+    it('should compile a single file', function (done) {
+        util.compileAndRunConfig({
+            files: [
+                'simple.c'
+            ]
+        }, './simple', done);
+    });
+
+    it('should compile to an executable with outputName', function (done) {
+        util.compileAndRunConfig({
+            files: [
+                'simple.c'
+            ],
+            outputName: 'my_executable'
+        }, './my_executable', done);
+    });
+});
