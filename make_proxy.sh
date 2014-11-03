92b996b60f86830aff4cafcec1d35b801bac919f
Jesper Oskarsson <jesosk@gmail.com>
2014-11-03 15:19:01 +0100

    Proxy shell script to run platform's Makefile

--- make_proxy.sh
+++ make_proxy.sh
@@ -9,7 +9,5 @@
 # You should have received a copy of the CC0 Public Domain Dedication along with this software.
 # If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
-all:
-	@dependencies/fetch_dependencies.sh
-	@echo Compiling...
-	@./make_proxy.sh
+platform=`uname -s | tr 'A-Z' 'a-z'`
+make -f Makefile.$platform
