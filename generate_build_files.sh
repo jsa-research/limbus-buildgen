c60aeefb89db6d76b60a8987ceaa93cd3d64d4f6
Jesper Oskarsson <jesosk@gmail.com>
2014-11-05 23:18:47 +0100

    Added script for generating build files.
    Also added Makefile.coverage

---
+++ generate_build_files.sh
@@ -0,0 +1,16 @@
+
+# sea-strap.js - A "build anywhere" C/C++ makefile/project generator.
+# Written in 2014 by Jesper Oskarsson jesosk@gmail.com
+#
+# To the extent possible under law, the author(s) have dedicated all copyright
+# and related and neighboring rights to this software to the public domain worldwide.
+# This software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication along with this software.
+# If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+./duk sea-strap.js freebsd > Makefile.freebsd
+./duk sea-strap.js linux > Makefile.linux
+./duk sea-strap.js darwin > Makefile.darwin
+./duk sea-strap.js win32 > Makefile.mak
+./duk sea-strap.js linux -g -O0 -coverage > Makefile.coverage
