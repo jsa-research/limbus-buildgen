c44e7588f3a288c49ec584eb940c22300cdb2932
Jesper Oskarsson <jesosk@gmail.com>
2014-11-05 23:18:13 +0100

    Passing in extra compiler flags to the front end now works

--- sea-strap.js
+++ sea-strap.js
@@ -9,24 +9,29 @@
 // You should have received a copy of the CC0 Public Domain Dedication along with this software.
 // If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
-var duktape_version = "1.0.1";
+var duktape_version = '1.0.1';
 var makefile_generator = require('./source/makefile-generator');
 
 var printHosts = function () {
-    console.log("\nSupported hosts are:");
+    console.log('\nSupported hosts are:');
 
     makefile_generator.supportedHosts.forEach(function (host) {
-        console.log("  * " + host);
+        console.log('  * ' + host);
     });
 
-    console.log("");
+    console.log('');
 };
 
 if (process.argv.length < 3) {
-    console.log("Missing host");
+    console.log('Missing host');
     printHosts();
 } else {
     var host = process.argv[2];
+    
+    var compilerFlags;
+    if (process.argv.length > 3) {
+        compilerFlags = process.argv.slice(3).join(' ');
+    }
 
     if (makefile_generator.supportedHosts.indexOf(host) !== -1) {
         console.log(makefile_generator.generate({
@@ -39,10 +44,11 @@ if (process.argv.length < 3) {
                 'dependencies/duktape-' + duktape_version + '/src/'
             ],
             outputName: 'duk',
+            compilerFlags: compilerFlags,
             host: host
         }));
     } else {
-        console.log("Invalid host");
+        console.log('Invalid host');
         printHosts();
     }
 }
