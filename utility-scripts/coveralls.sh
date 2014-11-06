16dd82c15b8ab4daf83324bd36948e099498f15e
Jesper Oskarsson <jesosk@gmail.com>
2014-11-06 02:37:11 +0100

    Tried something better

--- utility-scripts/coveralls.sh
+++ utility-scripts/coveralls.sh
@@ -9,21 +9,15 @@
 # You should have received a copy of the CC0 Public Domain Dedication along with this software.
 # If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
-# Install dependencies
-sudo apt-get update
-sudo apt-get install lcov ruby2.0
-sudo gem install coveralls-lcov
-
 # C coverage
 make -f Makefile.coverage
 ./duk sea-strap.js linux
-lcov -b . -d . -c -o duk.info
-lcov --extract duk.info "`pwd -P`/source/*" -o duk.info
-coveralls-lcov -n -v duk.info > duk.json
+lcov -b . -d . -c -o c-coverage.info
+lcov --extract c-coverage.info "`pwd -P`/source/*" -o c-coverage.info
 
 # Javascript coverage
-mocha --require blanket -R mocha-lcov-reporter \"**/*.unit.js\" \"**/*.feature.js\" > c-coverage.json
+mocha --require blanket -R mocha-lcov-reporter \"**/*.unit.js\" \"**/*.feature.js\" > javascript-coverage.info
 
 # Merge coverage files
-node ./utility-scripts/merge-coverage.js javascript-coverage.json duk.json > coverage.json
-cat coverage.json | ./node_modules/coveralls/bin/coveralls.js
+./node_modules/lcov-result-merger/bin/lcov-result-merger.js *-coverage.info coverage.info
+cat coverage.info | ./node_modules/coveralls/bin/coveralls.js
