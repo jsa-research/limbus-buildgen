1890534af1708006b9ea7a55b5bcb5a6b6c053df
Jesper Oskarsson <jesosk@gmail.com>
2014-11-06 02:38:01 +0100

    Need lcov

--- utility-scripts/coveralls.sh
+++ utility-scripts/coveralls.sh
@@ -9,6 +9,8 @@
 # You should have received a copy of the CC0 Public Domain Dedication along with this software.
 # If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
+sudo apt-get install lcov
+
 # C coverage
 make -f Makefile.coverage
 ./duk sea-strap.js linux
