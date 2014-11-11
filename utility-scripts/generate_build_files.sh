3db2e23447e824aaa3339cc9ea47db69332a352f
Jesper Oskarsson <jesosk@gmail.com>
2014-11-11 19:54:41 +0100

    Updated build config files and build file generator script

--- utility-scripts/generate_build_files.sh
+++ utility-scripts/generate_build_files.sh
@@ -9,10 +9,10 @@
 # You should have received a copy of the CC0 Public Domain Dedication along with this software.
 # If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
-./duk sea-strap.js build_configs/freebsd.json > Makefile.freebsd
-./duk sea-strap.js build_configs/linux.json > Makefile.linux
-./duk sea-strap.js build_configs/darwin.json > Makefile.darwin
-./duk sea-strap.js build_configs/win32.json > Makefile.mak
+./duk sea-strap.js --buildFile Makefile.freebsd --host freebsd build_configs/build.json
+./duk sea-strap.js --buildFile Makefile.linux --host linux build_configs/build.json
+./duk sea-strap.js --buildFile Makefile.darwin --host darwin build_configs/build.json
+./duk sea-strap.js --buildFile Makefile.mak --host win32 build_configs/build.json
 
 # This is used for sending coverage information from the C sources to coveralls.io
-./duk sea-strap.js build_configs/coverage.json > Makefile.coverage
+./duk sea-strap.js --buildFile Makefile.coverage build_configs/coverage.json
