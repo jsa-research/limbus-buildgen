1f084e2975c65ada907cbd06237f9c17e90e5145
Jesper Oskarsson <jesosk@gmail.com>
2016-03-19 13:19:50 +0100

    Added missing $ in fetch_dependencies.ps1

--- dependencies/fetch_dependencies.ps1
+++ dependencies/fetch_dependencies.ps1
@@ -4,15 +4,14 @@ $duktape_version="1.4.0"
 
 # Global variables
 $current_directory = (Get-Location).ToString()
-
-# Duktape download
 $dependencies_folder_relative = "\dependencies"
 $dependencies_folder = $current_directory + $dependencies_folder_relative
 
+# Duktape download
 $duktape_archive_url = "https://github.com/svaarala/duktape-releases/archive/v" + $duktape_version + ".zip"
 $duktape_archive_destination = $dependencies_folder + "\duktape-" + $duktape_version + ".zip"
-$duktape_final_directory = dependencies_folder + "\duktape-" + $duktape_version
-$duktape_intermediary_directory = dependencies_folder + "\duktape-releases-" + $duktape_version
+$duktape_final_directory = $dependencies_folder + "\duktape-" + $duktape_version
+$duktape_intermediary_directory = $dependencies_folder + "\duktape-releases-" + $duktape_version
 
 Invoke-WebRequest $duktape_archive_url -OutFile $duktape_archive_destination
 [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
