29368bd0ed5cc5725c447c0d99a00048d8256f08
Jesper Oskarsson <jesosk@gmail.com>
2016-03-19 12:59:14 +0100

    Rename duktape release folder on windows as the folder name changed

--- dependencies/fetch_dependencies.ps1
+++ dependencies/fetch_dependencies.ps1
@@ -5,4 +5,5 @@ if (Test-Path .\dependencies\duktape-1.4.0) {
     Remove-Item .\dependencies\duktape-1.4.0 -Force -Recurse
 }
 [System.IO.Compression.ZipFile]::ExtractToDirectory((Get-Location).ToString() + '\dependencies\duktape-1.4.0.zip', (Get-Location).ToString() + '\dependencies')
+Move-Item .\dependencies\duktape-releases-1.4.0 .\dependencies\duktape-1.4.0
 Remove-Item .\dependencies\duktape-1.4.0.zip -Force
