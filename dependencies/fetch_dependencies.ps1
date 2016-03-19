527665edb9e8071b7cf2d9d0893b14bce6a7cdb9
Jesper Oskarsson <jesosk@gmail.com>
2016-03-19 13:11:43 +0100

    Refactored fetch_dependencies.ps1

--- dependencies/fetch_dependencies.ps1
+++ dependencies/fetch_dependencies.ps1
@@ -1,9 +1,24 @@
 
-Invoke-WebRequest "https://github.com/svaarala/duktape-releases/archive/v1.4.0.zip" -OutFile ".\dependencies\duktape-1.4.0.zip"
+# Versions
+$duktape_version="1.4.0"
+
+# Global variables
+$current_directory = (Get-Location).ToString()
+
+# Duktape download
+$dependencies_folder_relative = "\dependencies"
+$dependencies_folder = $current_directory + $dependencies_folder_relative
+
+$duktape_archive_url = "https://github.com/svaarala/duktape-releases/archive/v" + $duktape_version + ".zip"
+$duktape_archive_destination = $dependencies_folder + "\duktape-" + $duktape_version + ".zip"
+$duktape_final_directory = dependencies_folder + "\duktape-" + $duktape_version
+$duktape_intermediary_directory = dependencies_folder + "\duktape-releases-" + $duktape_version
+
+Invoke-WebRequest $duktape_archive_url -OutFile $duktape_archive_destination
 [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
-if (Test-Path .\dependencies\duktape-1.4.0) {
-    Remove-Item .\dependencies\duktape-1.4.0 -Force -Recurse
+if (Test-Path $duktape_final_directory) {
+    Remove-Item $duktape_final_directory -Force -Recurse
 }
-[System.IO.Compression.ZipFile]::ExtractToDirectory((Get-Location).ToString() + '\dependencies\duktape-1.4.0.zip', (Get-Location).ToString() + '\dependencies')
-Move-Item .\dependencies\duktape-releases-1.4.0 .\dependencies\duktape-1.4.0
-Remove-Item .\dependencies\duktape-1.4.0.zip -Force
+[System.IO.Compression.ZipFile]::ExtractToDirectory(duktape_archive_destination, $dependencies_folder)
+Move-Item $duktape_intermediary_directory $duktape_final_directory
+Remove-Item $duktape_archive_destination -Force
