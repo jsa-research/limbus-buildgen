147e0748d89c12e32e64f3fd9f9752362427fc94
Jesper Oskarsson <jesosk@gmail.com>
2016-03-19 12:47:34 +0100

    Download correct duktape dependency for windows

--- dependencies/fetch_dependencies.ps1
+++ dependencies/fetch_dependencies.ps1
@@ -1,5 +1,5 @@
 
-Invoke-WebRequest "https://github.com/svaarala/duktape/archive/v1.4.0.zip" -OutFile ".\dependencies\duktape-1.4.0.zip"
+Invoke-WebRequest "https://github.com/svaarala/duktape-releases/archive/v1.4.0.zip" -OutFile ".\dependencies\duktape-1.4.0.zip"
 [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
 if (Test-Path .\dependencies\duktape-1.4.0) {
     Remove-Item .\dependencies\duktape-1.4.0 -Force -Recurse
