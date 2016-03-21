a3290dddf424ba10ad2148757e107dd0d6593db8
Jesper Oskarsson <jesosk@gmail.com>
2016-03-21 14:46:04 +0100

    Fixed parsing issue in .travis.yml

---
+++ utility-scripts/echo-versions.sh
@@ -0,0 +1,22 @@
+
+echo NVM version:
+nvm --version
+echo
+echo Node.js version:
+node -v
+echo
+echo NPM version:
+npm -v
+echo
+echo uname output:
+uname -rsv
+echo
+echo GCC version:
+gcc -v
+echo
+echo Clang version:
+clang -v
+echo
+echo LD version:
+ld -v
+echo
