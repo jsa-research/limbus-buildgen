5c8cc749aa5368c7e3cb7320f91f3395f252133c
Jesper Oskarsson <jesosk@gmail.com>
2016-04-24 23:45:10 +0200

    Modified ci_script to take http serve path

--- utility-scripts/ci_script.sh
+++ utility-scripts/ci_script.sh
@@ -1,14 +1,13 @@
 
 # Create a simple CI server by running:
 #
-# npm install node-static githubhook
-# static -p40251 ci_public &
-# githubhook -port=40252 --secret=GITHUB_SECRET push:node-github-hook ./utility-scripts/ci_script.sh
+# npm install githubhook -g
+# githubhook -port=40252 --secret=GITHUB_SECRET push:node-github-hook ./utility-scripts/ci_script.sh HTTP_SERVED_DIRECTORY
 #
 
-mkdir -p ci_public
-npm test > ci_public/ci-test.log
+mkdir -p $1
+npm test > $1/ci-test.log
 if [ $? -eq 0 ]
-then curl -sL http://img.shields.io/badge/build-passing-brightgreen.svg > ci_public/badge.svg
-else curl -sL http://img.shields.io/badge/build-failing-red.svg > ci_public/badge.svg
+then curl -sL http://img.shields.io/badge/build-passing-brightgreen.svg > $1/badge.svg
+else curl -sL http://img.shields.io/badge/build-failing-red.svg > $1/badge.svg
 fi
