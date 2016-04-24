1a1fddc7d74f52ce4077e2c9bf05d9d0e944c08f
Jesper Oskarsson <jesosk@gmail.com>
2016-04-25 00:32:49 +0200

    Changed ci test log file ending to .txt so that it can be read in browser

--- utility-scripts/ci_script.sh
+++ utility-scripts/ci_script.sh
@@ -6,7 +6,7 @@
 #
 
 mkdir -p $1
-npm test > $1/ci-test.log
+npm test > $1/ci-test.txt
 if [ $? -eq 0 ]
 then curl -sL http://img.shields.io/badge/build-passing-brightgreen.svg > $1/badge.svg
 else curl -sL http://img.shields.io/badge/build-failing-red.svg > $1/badge.svg
