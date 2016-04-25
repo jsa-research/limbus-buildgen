bd05a7b00dcaa58afef8969d96c1aaebdf00749b
Jesper Oskarsson <jesosk@gmail.com>
2016-04-25 11:07:58 +0200

    Improved ci_script.sh to install dependencies and show versions

--- utility-scripts/ci_script.sh
+++ utility-scripts/ci_script.sh
@@ -1,4 +1,15 @@
 
+# limbus-buildgen - A "build anywhere" C/C++ makefile/project generator.
+# Written in 2016 by Jesper Oskarsson jesosk@gmail.com
+#
+# To the extent possible under law, the author(s) have dedicated all copyright
+# and related and neighboring rights to this software to the public domain worldwide.
+# This software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication along with this software.
+# If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+
 # Create a simple CI server by running:
 #
 # npm install githubhook -g
@@ -14,10 +25,19 @@ CI_FAILING_BADGE=http://img.shields.io/badge/build-failing-red.svg
 
 mkdir -p $CI_SCRIPT_WWW_PATH
 echo Starting CI Build at $CI_DATE > $CI_LOG
+echo BUILDGEN_NODE_VERSION:
+echo $BUILDGEN_NODE_VERSION
+echo BUILDGEN_TARGET_COMPILER:
+echo $BUILDGEN_TARGET_COMPILER
+. utility-scripts/install_node.sh $BUILDGEN_NODE_VERSION
+. utility-scripts/echo_versions.sh
 echo Pulling new changes... >> $CI_LOG
 git pull >> $CI_LOG
+echo Installing dependencies... >> $CI_LOG
+npm install
+dependencies/fetch_dependencies.sh
 echo Running tests... >> $CI_LOG
-npm test >> $CI_LOG
+npm run test -- -t 20000 && npm run coverage-travis-ci >> $CI_LOG
 if [ $? -eq 0 ]
 then curl -sL $CI_PASSING_BADGE > $CI_BADGE ; echo TESTS PASSING! >> $CI_LOG
 else curl -sL $CI_FAILING_BADGE > $CI_BADGE ; echo TESTS FAILING! >> $CI_LOG
