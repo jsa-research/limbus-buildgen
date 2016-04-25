c532778ba98c369d7641df5a57e4633f6cabc5a6
Jesper Oskarsson <jesosk@gmail.com>
2016-04-25 10:15:01 +0200

    Fixed incorrect variable assignment in ci_script.sh

--- utility-scripts/ci_script.sh
+++ utility-scripts/ci_script.sh
@@ -6,11 +6,11 @@
 # githubhook --secret=GITHUB_SECRET push:limbus-buildgen ./utility-scripts/ci_script.sh
 #
 
-$CI_LOG=$CI_SCRIPT_WWW_PATH/ci-log.txt
-$CI_DATE=`date`
-$CI_BADGE=$CI_SCRIPT_WWW_PATH/badge.svg
-$CI_PASSING_BADGE=http://img.shields.io/badge/build-passing-brightgreen.svg
-$CI_FAILING_BADGE=http://img.shields.io/badge/build-failing-red.svg
+CI_LOG=$CI_SCRIPT_WWW_PATH/ci-log.txt
+CI_DATE=`date`
+CI_BADGE=$CI_SCRIPT_WWW_PATH/badge.svg
+CI_PASSING_BADGE=http://img.shields.io/badge/build-passing-brightgreen.svg
+CI_FAILING_BADGE=http://img.shields.io/badge/build-failing-red.svg
 
 mkdir -p $CI_SCRIPT_WWW_PATH
 echo Starting CI Build at $CI_DATE > $CI_LOG
