0804b104799d695b124fd1d1eea7cbabc3e6aba1
Jesper Oskarsson <jesosk@gmail.com>
2016-04-25 10:10:28 +0200

    Improved ci_script.sh to output more information and to actually pull new changes.

--- utility-scripts/ci_script.sh
+++ utility-scripts/ci_script.sh
@@ -2,13 +2,23 @@
 # Create a simple CI server by running:
 #
 # npm install githubhook -g
-# set CI_SCRIPT_WWW_PATH=...
-# githubhook -port=40252 --secret=GITHUB_SECRET push:limbus-buildgen ./utility-scripts/ci_script.sh
+# CI_SCRIPT_WWW_PATH=...
+# githubhook --secret=GITHUB_SECRET push:limbus-buildgen ./utility-scripts/ci_script.sh
 #
 
+$CI_LOG=$CI_SCRIPT_WWW_PATH/ci-log.txt
+$CI_DATE=`date`
+$CI_BADGE=$CI_SCRIPT_WWW_PATH/badge.svg
+$CI_PASSING_BADGE=http://img.shields.io/badge/build-passing-brightgreen.svg
+$CI_FAILING_BADGE=http://img.shields.io/badge/build-failing-red.svg
+
 mkdir -p $CI_SCRIPT_WWW_PATH
-npm test > $CI_SCRIPT_WWW_PATH/ci-test.txt
+echo Starting CI Build at $CI_DATE > $CI_LOG
+echo Pulling new changes... >> $CI_LOG
+git pull >> $CI_LOG
+echo Running tests... >> $CI_LOG
+npm test >> $CI_LOG
 if [ $? -eq 0 ]
-then curl -sL http://img.shields.io/badge/build-passing-brightgreen.svg > $CI_SCRIPT_WWW_PATH/badge.svg
-else curl -sL http://img.shields.io/badge/build-failing-red.svg > $CI_SCRIPT_WWW_PATH/badge.svg
+then curl -sL $CI_PASSING_BADGE > $CI_BADGE ; echo TESTS PASSING! >> $CI_LOG
+else curl -sL $CI_FAILING_BADGE > $CI_BADGE ; echo TESTS FAILING! >> $CI_LOG
 fi
