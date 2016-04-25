4c8a501e24f7ac796e133bc2db5e6df89eec31b3
Jesper Oskarsson <jesosk@gmail.com>
2016-04-25 11:55:39 +0200

    Added several ci builds in ci_script.sh

--- utility-scripts/ci_script.sh
+++ utility-scripts/ci_script.sh
@@ -18,30 +18,50 @@
 # githubhook --secret=GITHUB_SECRET push:limbus-buildgen ./utility-scripts/ci_script.sh
 #
 
-CI_LOG=$CI_SCRIPT_WWW_PATH/ci-log.txt
-CI_DATE=`date`
-CI_BADGE=$CI_SCRIPT_WWW_PATH/badge.svg
-CI_PASSING_BADGE=http://img.shields.io/badge/build-passing-brightgreen.svg
-CI_FAILING_BADGE=http://img.shields.io/badge/build-failing-red.svg
+git pull
 
-mkdir -p $CI_SCRIPT_WWW_PATH
-echo Starting CI Build at $CI_DATE > $CI_LOG
-echo BUILDGEN_NODE_VERSION: >> $CI_LOG
-echo $BUILDGEN_NODE_VERSION >> $CI_LOG
-echo >> $CI_LOG
-echo BUILDGEN_TARGET_COMPILER: >> $CI_LOG
-echo $BUILDGEN_TARGET_COMPILER >> $CI_LOG
-echo >> $CI_LOG
-. utility-scripts/install_node.sh $BUILDGEN_NODE_VERSION >> $CI_LOG 2>&1
-. utility-scripts/echo_versions.sh >> $CI_LOG 2>&1
-echo Pulling new changes... >> $CI_LOG
-git pull >> $CI_LOG 2>&1
-echo Installing dependencies... >> $CI_LOG
-npm install >> $CI_LOG 2>&1
-dependencies/fetch_dependencies.sh >> $CI_LOG 2>&1
-echo Running tests... >> $CI_LOG
-npm run test -- -t 20000 >> $CI_LOG 2>&1
-if [ $? -eq 0 ]
-then curl -sL $CI_PASSING_BADGE > $CI_BADGE ; echo TESTS PASSING! >> $CI_LOG
-else curl -sL $CI_FAILING_BADGE > $CI_BADGE ; echo TESTS FAILING! >> $CI_LOG
-fi
+run_ci()
+{
+    CI_SCRIPT_WWW_PATH=$ORIGINAL_CI_SCRIPT_WWW_PATH/node$BUILDGEN_NODE_VERSION-$BUILDGEN_TARGET_COMPILER
+
+    CI_LOG=$CI_SCRIPT_WWW_PATH/ci-log.txt
+    CI_DATE=`date`
+    CI_BADGE=$CI_SCRIPT_WWW_PATH/badge.svg
+    CI_PASSING_BADGE=http://img.shields.io/badge/build-passing-brightgreen.svg
+    CI_FAILING_BADGE=http://img.shields.io/badge/build-failing-red.svg
+
+    mkdir -p $CI_SCRIPT_WWW_PATH
+    echo Starting CI Build at $CI_DATE > $CI_LOG
+    echo BUILDGEN_NODE_VERSION: >> $CI_LOG
+    echo $BUILDGEN_NODE_VERSION >> $CI_LOG
+    echo >> $CI_LOG
+    echo BUILDGEN_TARGET_COMPILER: >> $CI_LOG
+    echo $BUILDGEN_TARGET_COMPILER >> $CI_LOG
+    echo >> $CI_LOG
+    . utility-scripts/install_node.sh $BUILDGEN_NODE_VERSION >> $CI_LOG 2>&1
+    . utility-scripts/echo_versions.sh >> $CI_LOG 2>&1
+    echo Testing commit: >> $CI_LOG
+    echo git log -n 1 >> $CI_LOG 2>&1
+    echo Installing dependencies... >> $CI_LOG
+    npm install >> $CI_LOG 2>&1
+    dependencies/fetch_dependencies.sh >> $CI_LOG 2>&1
+    echo Running tests... >> $CI_LOG
+    npm run test -- -t 20000 >> $CI_LOG 2>&1
+    if [ $? -eq 0 ]
+    then curl -sL $CI_PASSING_BADGE > $CI_BADGE ; echo TESTS PASSING! >> $CI_LOG
+    else curl -sL $CI_FAILING_BADGE > $CI_BADGE ; echo TESTS FAILING! >> $CI_LOG
+    fi
+}
+
+export BUILDGEN_NODE_VERSION
+export BUILDGEN_TARGET_COMPILER
+
+ORIGINAL_CI_SCRIPT_WWW_PATH=$CI_SCRIPT_WWW_PATH
+
+BUILDGEN_NODE_VERSION=5.9.0
+BUILDGEN_TARGET_COMPILER=clang
+run_ci
+
+BUILDGEN_NODE_VERSION=4.4.0
+BUILDGEN_TARGET_COMPILER=clang
+run_ci
